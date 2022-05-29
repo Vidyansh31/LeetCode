51f9ea600c7d39e5e94313b4ddd1ddc81122af69
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-05-29 18:38:26 +0530

    Time: 2 ms (15.14%), Space: 41.6 MB (67.04%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -16,44 +16,37 @@
 class Solution {
     
     public boolean isCousins(TreeNode root, int x, int y) {
-        List<Integer> X = nodeToRootPath(root,x);
-        List<Integer> Y = nodeToRootPath(root,y);
-        
-        if(X.size() > 1 && Y.size() > 1 && X.get(1) == Y.get(1)){
+        if(root == null){
             return false;
         }
-        
-        int x_depth = X.size()-1;
-        int y_depth = Y.size()-1;
-     
-        return x_depth == y_depth ? true :false;
-    }
-    
-    
-    public List<Integer> nodeToRootPath(TreeNode root, int key){
-        if(root == null){
-            return new ArrayList<>();
+        Queue<TreeNode> qu = new LinkedList<>();
+        qu.add(root);
+        int depthX = -1;
+        int depthY = -2;
+        int level = 0;
+        while(qu.size() != 0){
+            int size = qu.size();
+            for(int i = 0; i <size; i++){
+                TreeNode rem = qu.remove();
+                
+                if(rem.left != null && rem.right != null){
+                    if(rem.left.val == x && rem.right.val == y) return false;
+                    if(rem.left.val == y && rem.right.val == x) return false;
+                }
+                
+                if(rem.val == x) depthX = level;
+                if(rem.val == y) depthY = level;
+                
+                if(rem.left != null){
+                    qu.add(rem.left);
+                }
+                if(rem.right != null){
+                    qu.add(rem.right);
+                }
+            }
+            level++;
         }
         
-        if(root.val == key){
-            List<Integer> base = new ArrayList<>();
-            base.add(root.val);
-            return base;
-        }
-        
-        List<Integer> left = nodeToRootPath(root.left,key);
-        if(left.size() > 0){
-            left.add(root.val);
-            return left;
-        }
-        
-        List<Integer> right = nodeToRootPath(root.right,key);
-        if(right.size() > 0){
-            right.add(root.val);
-            return right;
-        }
-        
-        return new ArrayList<>();
-    }
-    
+         return depthX == depthY;
+    } 
 }
