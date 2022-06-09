4ed427f7d2c2380b8d7f33ff34430e78014c6589
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-06-09 15:38:47 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (70.70%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,6 +1,35 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int countZero = 0;
+        int countOne = 0;
+        int countTwo = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                countZero++;
+            }
+            else if(nums[i] == 1){
+                countOne++;
+            }
+            else{
+                countTwo++;
+            }
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(countZero > 0){
+                nums[i] = 0;
+                countZero--;
+            }
+            else if(countOne > 0){
+                nums[i] = 1;
+                countOne--;
+            }
+            else{
+                nums[i] = 2;
+                countTwo--;
+            }
+        }
         
     }
 }
