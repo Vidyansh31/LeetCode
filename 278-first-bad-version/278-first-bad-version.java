d0f61b510468d59ea12bd262abf9813c9d6747b6
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-06-21 11:22:22 +0530

    Time: 26 ms (25.19%), Space: 40.6 MB (63.24%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,6 +3,14 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
+        // If nth version is not a bad version, then all versions are good.
+        if (n <= 0 || !isBadVersion(n)) {
+            return -1;
+        }
+        // If first version is a bad version, then all versions are bad.
+        if (isBadVersion(1)) {
+            return 1;
+        }
         int low = 1;
         int high = n;
         int rv = -1;
