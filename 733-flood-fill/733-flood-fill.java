8ddbb7624701813aeba271de0f87b1bfa6b54508
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-06-29 13:35:19 +0530

    Time: 1 ms (89.89%), Space: 48 MB (56.58%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,24 +1,25 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-            int gridColor = image[sr][sc];
-            if(gridColor == color){
-                return image;
-            }
-            dfs(image,sr,sc,color,gridColor);
+        int gridColor = image[sr][sc];
+        if(gridColor == color){
+            return image;
+        }
+        
+        dfs(image,sr,sc,color,gridColor);
+        
         return image;
     }
     
-    public void dfs(int[][] image, int sr, int sc, int color, int gridColor){
-        if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != gridColor){
+    public void dfs(int[][] image, int i , int j , int color , int gridColor){
+        if(i < 0 || j < 0 || i >= image.length || j >= image[0].length || image[i][j] != gridColor){
             return;
         }
         
+        image[i][j] = color;
         
-        image[sr][sc] = color;
-        
-        dfs(image,sr-1,sc,color,gridColor);
-        dfs(image,sr,sc+1,color,gridColor);
-        dfs(image,sr+1,sc,color,gridColor);
-        dfs(image,sr,sc-1,color,gridColor);
+        dfs(image,i-1,j,color,gridColor);
+        dfs(image,i,j+1,color,gridColor);
+        dfs(image,i+1,j,color,gridColor);
+        dfs(image,i,j-1,color,gridColor);
     }
 }
