aee67fd810d1481aefd90f55fc4cd8a1b19fa518
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-04 13:15:12 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.java
+++ Merge Without Extra Space - GFG/merge-without-extra-space.java
@@ -46,7 +46,7 @@ class Solution {
 	   int j = 0;
 	   
 	   while(i >= 0 && j < m){
-	       if(arr1[i] > arr2[j]){
+	       if(arr1[i] >= arr2[j]){
 	           int temp = arr1[i];
 	           arr1[i] = arr2[j];
 	           arr2[j] = temp;
