e15b047da6bcfdf5a2b3243a9946e7a676fa21c7
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-04 11:31:18 +0530

    Time: 5 ms (37.46%), Space: 68.4 MB (32.06%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,23 +1,31 @@
 class Solution {
     public boolean canJump(int[] arr) {
-         int[] dp = new int[arr.length];
-        Arrays.fill(dp,-1);
-        dp[0] = 0;
-        
-        for(int i = 1; i < arr.length; i++){
-            for(int j = 0; j < i; j++){
-                if(dp[j] == -1){
-                    return false;
-                }
-                else if(arr[j]+j >= i){
-                    dp[i] = dp[j]+1;
-                    break;
-                }
-            }
+         if(arr.length == 1){
+            return true;
         }
         
-        if(dp[arr.length -1] != -1){
-            return true;
+        if(arr[0] == 0){
+            return false;
+        }
+        
+        int range = arr[0];
+        int step = arr[0];
+        int jump = 1;
+        
+        for(int i = 1; i < arr.length; i++){
+            if( i == arr.length-1){
+                return true;
+            }
+            
+            range = Math.max(range,i+arr[i]);
+            step--;
+            if(step == 0){
+                jump++;
+                if(range <= i){
+                    return false;
+                }
+                step = range - i;
+            }
         }
         
         return false;
