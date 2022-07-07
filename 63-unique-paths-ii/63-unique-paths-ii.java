2412fbb02a0391d91ba7f3570faee1ea153958ad
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-07 15:55:50 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (82.90%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -3,28 +3,33 @@ class Solution {
         int n = obstacleGrid.length-1;
         int m = obstacleGrid[0].length-1;
         int[][] dp = new int[n+1][m+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < m+1; j++){
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else if(i == 0 && j == 0){
+                    //their is only 1 way to reach (0,0) from (0,0);
+                     dp[i][j] = 1;
+        
+                }
+                else if(i == 0){
+                    dp[i][j] = dp[i][j-1];
+                }
+                else if(j == 0){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                }
+            }
         }
-        return helper(obstacleGrid,0,0,n,m,dp);
+        
+        
+        return dp[n][m];
+        
     }
     
-    private int helper(int[][] grid, int sr, int sc, int dr, int dc,int[][] dp){
-        if(sr > dr || sc > dc || grid[sr][sc] == 1){
-            return 0;
-        }
-        
-        if(sr == dr && sc == dc){
-            return 1;
-        }
-        
-        if(dp[sr][sc] != -1){
-            return dp[sr][sc];
-        }
-        
-        int right = helper(grid,sr,sc+1,dr,dc,dp);
-        int left = helper(grid,sr+1,sc,dr,dc,dp);
-        
-        return dp[sr][sc] = right+left;
-    }
+   
 }
