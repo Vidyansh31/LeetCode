d3b88097ddfa98dc31d325702651f3ef38086e13
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-09 00:47:11 +0530

    Time: 130 ms (6.08%), Space: 48 MB (29.67%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,47 +1,52 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
+        //As row and two columns are changing we need 3D dp
         int[][][] dp = new int[grid.length][grid[0].length][grid[0].length];
-        for(int[][] a : dp){
-            for(int[] row : a){
-                Arrays.fill(row,-1);
-            }
-        }
         
-        return helper(grid,0,0,grid[0].length-1,dp);
-    }
-    
-    private int helper(int[][] grid, int r1, int c1, int c2, int[][][] dp){
-        if(c1 < 0 || c2 < 0 || c1 >= grid[0].length || c2 >= grid[0].length){
-            return -(int)1e9;
-        }
+        //We start from n-1 and go to 0 row 
         
-        if(r1 == grid.length-1){
-            if(c1 == c2){
-                return grid[r1][c1];
-            }
-            else{
-                return grid[r1][c1]+grid[r1][c2];
-            }
-        }
-        
-        if(dp[r1][c1][c2] != -1){
-            return dp[r1][c1][c2];
-        }
-        
-        int maxi = 0;
-        for(int i = -1; i <= 1; i++){
-            for(int j = -1; j <= 1; j++){
-                if(c1==c2){
-                    int rec = grid[r1][c1] + helper(grid,r1+1,c1+i,c2+j, dp);
-                    maxi = Math.max(rec,maxi);
+        for(int i = grid.length-1; i >= 0 ; i--){
+            for(int j1 = 0; j1 < grid[0].length; j1++){
+                for(int j2 = 0; j2 < grid[0].length; j2++){
+                   if(i == grid.length-1){
+                        if(j1 == j2){
+                            dp[i][j1][j2] = grid[i][j1];
+                        }
+                        else{
+                            dp[i][j1][j2] = grid[i][j1]+grid[i][j2];
+                        }
+                    }
+                    else {
+                         int maxi = 0;
+                        for(int dj1 = -1; dj1 <= 1; dj1++){
+                            for(int dj2 = -1; dj2 <= 1; dj2++){
+                                int value = 0;
+                                if(j1+dj1 >= 0 && j1+dj1 < grid[0].length && j2+dj2 >= 0 && j2+dj2 < grid[0].length ){
+                                    if(j1 == j2){
+                                        value = grid[i][j1];
+                                    }
+                                    else{
+                                        value = grid[i][j1]+ grid[i][j2]; }
+                                    
+                                    value += dp[i+1][j1+dj1][j2+dj2];
+                                }
+                                else{
+                                    value = -(int)1e9;
+                                }
+                                
+                                maxi = Math.max(value,maxi);
+                            }
+                        }
+                        
+                        dp[i][j1][j2] = maxi;
+                    }
                 }
-                else{
-                    int rec = grid[r1][c1]+ grid[r1][c2] + helper(grid,r1+1,c1+i,c2+j,dp);
-                    maxi = Math.max(rec,maxi);
-                }
             }
         }
         
-        return dp[r1][c1][c2] = maxi;
+        
+        return dp[0][0][grid[0].length-1];
+        
     }
-}
+    }
+  
