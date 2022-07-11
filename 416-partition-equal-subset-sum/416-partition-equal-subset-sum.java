7375c89a45a2b0cf6dc1af4bc4126a6478c31f3e
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-11 13:59:51 +0530

    Time: 70 ms (47.79%), Space: 53.8 MB (60.54%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -12,29 +12,29 @@ class Solution {
         
         sum = sum/2;
         
-        boolean[][] dp = new boolean[nums.length][sum+1];
+        boolean[] prev = new boolean[sum+1];
+        prev[0] = true;
         
-        //base cases when tar == 0 then it is true;
-        for(int i = 0; i < nums.length; i++){
-            dp[i][0] = true;
-        }
-        //2nd case when we are on zero index and we have to search for nums[0] we store true;
-        // dp[0][nums[0]] = true;
+        
         
         for(int i = 1; i < nums.length; i++){
+            boolean[] curr = new boolean[sum+1];
+            curr[0] = true;
             for(int j = 1; j <= sum; j++){
-                 boolean notPick = dp[i-1][j];
+                 boolean notPick = prev[j];
                  boolean pick = false;
 
                  if(nums[i] <= j){
-                    pick = dp[i-1][j-nums[i]];
+                    pick = prev[j-nums[i]];
                  }
 
-                 dp[i][j] =  pick || notPick;
+                 curr[j] =  pick || notPick;
             }
+            
+            prev = curr;
         }
         
-        return dp[dp.length-1][dp[0].length-1];
+        return prev[sum];
         
     }
     
