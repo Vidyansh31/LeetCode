a1a58b50566fd5a1904191021115314deed0f578
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-13 15:24:46 +0530

    Time: 26 ms (32.03%), Space: 46.5 MB (35.27%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,28 +1,32 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int idx1 = text1.length()-1;
-        int idx2 = text2.length()-1;
-        Integer[][] dp = new Integer[idx1+1][idx2+1];
-        return helper(text1,idx1,text2,idx2,dp);
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n+1][m+1];
+        
+        //base cases 0th row and 0th column contains only zeroes
+        for(int j = 0; j <= m; j++){
+            dp[0][j] = 0;
+        }
+        
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = 0;
+        }
+        
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
     
-    private int helper(String s1, int idx1, String s2 , int idx2, Integer[][] dp){
-    
-        if(idx1 < 0 || idx2 < 0){
-            return 0;
-        }
-        
-        
-        if(dp[idx1][idx2] != null){
-            return dp[idx1][idx2];
-        }
-       
-        //when last char matches
-        if(s1.charAt(idx1) == s2.charAt(idx2)){
-            return dp[idx1][idx2] =  1 + helper(s1,idx1-1, s2, idx2-1,dp);
-        }
-        
-        return dp[idx1][idx2] =  Math.max(helper(s1,idx1-1,s2,idx2,dp),helper(s1, idx1, s2, idx2-1,dp));
-     
-    }
+   
 }
