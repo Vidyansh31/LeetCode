951279f01c32120ba8f9ef84bc06a7a42078adc8
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-14 16:08:42 +0530

    Time: 15 ms (91.74%), Space: 40.3 MB (99.05%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -3,30 +3,19 @@ class Solution {
         int n = s.length();
         int m = t.length();
         
-        int[][] dp = new int[n+1][m+1];
-        
-        //base cases
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = 1;
-        }
-        
-        for(int j = 1; j <= m ; j++){
-            dp[0][j] = 0;
-        }
-        
+       int[] prev = new int[m+1];
+        prev[0] = 1;
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
+            for(int j = m; j >= 1; j--){
                 if(s.charAt(i-1) == t.charAt(j-1)){
-                     dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
+                     prev[j] =  prev[j-1] + prev[j];
                 }
                   
             }
+            
         }
         
-        return dp[n][m];
+        return prev[m];
     }
     
    
