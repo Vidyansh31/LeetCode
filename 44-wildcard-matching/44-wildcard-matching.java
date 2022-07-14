c8c93ae89c8040bf2979a41714f9c7e475c92585
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-14 22:54:11 +0530

    Time: 38 ms (63.06%), Space: 48.8 MB (39.12%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -3,32 +3,36 @@ class Solution {
         int n = s.length();
         int m = p.length();
         
-        boolean[][] dp = new boolean[n+1][m+1];
+        boolean[] prev = new boolean[m+1];
         
-        dp[0][0] = true;
+        prev[0] = true;
        for(int j = 1 ; j <= m ; j++)
-            dp[0][j] = dp[0][j-1] && (p.charAt(j-1) == '*');
+            prev[j] = prev[j-1] && (p.charAt(j-1) == '*');
 
         
-        for(int i = 1; i <= n; i++){
-            dp[i][0] = false;
-        }
+        // for(int i = 1; i <= n; i++){
+        //     dp[i][0] = false;
+        // }
         
         for(int i = 1; i <= n; i++){
+            boolean[] curr = new boolean[m+1];
+            curr[0] = false;
             for(int j = 1; j <= m; j++){
                  if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
                 else if(p.charAt(j-1) == '*'){
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    curr[j] = prev[j] || curr[j-1];
                 }
                 else{
-                    dp[i][j] = false;
+                    curr[j] = false;
                 }
             }
+            
+            prev = curr;
         }
         
-        return dp[n][m];
+        return prev[m];
     }
     
     
