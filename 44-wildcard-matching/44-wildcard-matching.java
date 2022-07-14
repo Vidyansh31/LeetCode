35fb187dfd82af47290ba87e6f7430b30c10124c
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-14 22:40:44 +0530

    Time: 47 ms (45.59%), Space: 49 MB (36.23%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -3,43 +3,33 @@ class Solution {
         int n = s.length();
         int m = p.length();
         
-        Boolean[][] dp = new Boolean[n][m];
+        boolean[][] dp = new boolean[n+1][m+1];
         
-        return helper(s,p,n-1,m-1,dp);
-    }
-    
-    private boolean helper(String s, String p, int idx1, int idx2, Boolean[][] dp){
+        dp[0][0] = true;
+       for(int j = 1 ; j <= m ; j++)
+            dp[0][j] = dp[0][j-1] && (p.charAt(j-1) == '*');
+
         
-        if(idx1 < 0 && idx2 < 0){
-            return true;
+        for(int i = 1; i <= n; i++){
+            dp[i][0] = false;
         }
         
-        if(idx1 >= 0 && idx2 < 0){
-            return false;
-        }
-        
-        if(idx1 < 0 && idx2 >= 0){
-            for(int i = 0; i <=idx2 ; i++){
-                if(p.charAt(i) != '*'){
-                    return false;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                 if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else{
+                    dp[i][j] = false;
                 }
             }
-            return true;
         }
         
-        
-        if(dp[idx1][idx2] != null){
-            return dp[idx1][idx2];
-        }
-        
-        if(s.charAt(idx1) == p.charAt(idx2) || p.charAt(idx2) == '?'){
-            return dp[idx1][idx2] = helper(s,p,idx1-1,idx2-1,dp);
-        }
-        
-        if(p.charAt(idx2) == '*'){
-            return dp[idx1][idx2] = helper(s,p,idx1-1,idx2,dp) || helper(s,p,idx1,idx2-1,dp);
-        }
-        
-        return false;
+        return dp[n][m];
     }
+    
+    
 }
