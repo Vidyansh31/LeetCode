40096848b3c4e75f2c618b35ce80e20a74ce1d62
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-15 21:38:12 +0530

    Time: 0 ms (100.00%), Space: 39 MB (94.66%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int ans = 0;
+        
+        while(target > startValue){
+            ans++;
+            if(target%2 == 0){
+                target /=2;
+            }
+            else{
+                target += 1;
+            }
+        }
+        
+        return ans + startValue - target;
+    }
+}
