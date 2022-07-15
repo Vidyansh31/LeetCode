da5a4d1c774f3c6500dc38d16d40648e0ae649ac
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-15 15:52:17 +0530

    Time: 2 ms (53.71%), Space: 43.1 MB (11.61%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,28 +1,27 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        Integer[][] dp = new Integer[n][2];
-        return helper(prices, 0,1,dp);
+        int[][] dp = new int[n+2][2];
+        
+        
+        for(int i = n-1; i >= 0; i--){
+            for(int j = 0; j <= 1; j++){
+                int profit = 0;
+                if(j == 1){
+                    profit = Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                }
+                else{
+                    profit = Math.max(prices[i]+dp[i+2][1],dp[i+1][0]);
+                }
+
+
+                dp[i][j] = profit;
+            }
+        }
+        
+        
+        return dp[0][1];
     }
     
-    private int helper(int[] p, int i,int buy,Integer[][] dp){
-        if(i >= p.length){
-            return 0;
-        }
-        
-        if(dp[i][buy] != null){
-            return dp[i][buy];
-        }
-        
-        int profit = 0;
-        if(buy == 1){
-            profit = Math.max(-p[i]+helper(p,i+1,0,dp),helper(p,i+1,1,dp));
-        }
-        else{
-            profit = Math.max(p[i]+helper(p,i+2,1,dp),helper(p,i+1,0,dp));
-        }
-        
-        
-        return dp[i][buy] = profit;
-    }
+    
 }
