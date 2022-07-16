062cf3a766d7f8cd605f00514c9854f5983a1aae
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-16 12:34:18 +0530

    Time: 295 ms (8.34%), Space: 244.6 MB (5.06%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,29 +1,25 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        Integer[][] dp = new Integer[n][n+1];
-        return helper(nums,0,-1,dp);
+        int[][] dp = new int[n+1][n+1];
+        
+        for(int i = n-1; i >= 0; i--){
+            for(int j = i-1; j >= -1; j--){
+                //not take
+                int l1 =  dp[i+1][j+1];
+
+                int l2 = 0;
+
+                if(j == -1 || nums[i] > nums[j]){
+                    l2 = 1 + dp[i+1][i+1];
+                }
+
+                dp[i][j+1] = Math.max(l1,l2);
+            }
+        }
+        
+        return dp[0][0];
     }
     
-    private int helper(int[] nums, int i, int prev_i,Integer[][] dp){
-        
-        if(i == nums.length){
-            return 0;
-        }
-        
-        if(dp[i][prev_i+1] != null){
-            return dp[i][prev_i+1];
-        }
-        
-        //not take
-        int l1 = helper(nums, i+1,prev_i,dp);
-        
-        int l2 = 0;
-        
-        if(prev_i == -1 || nums[i] > nums[prev_i]){
-            l2 = 1 + helper(nums,i+1,i,dp);
-        }
-        
-        return dp[i][prev_i+1] = Math.max(l1,l2);
-    }
+   
 }
