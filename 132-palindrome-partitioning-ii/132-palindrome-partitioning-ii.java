a26017387a460d140ad2d14d060468ad42674372
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-17 22:55:00 +0530

    Time: 1835 ms (14.67%), Space: 42.5 MB (72.08%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -1,32 +1,27 @@
 class Solution {
     public int minCut(String s) {
-        Integer[] dp = new Integer[s.length()+1];
-        return helper(0,s,dp) - 1;
+        int[] dp = new int[s.length()+1];
+        
+        for(int i = s.length()-1; i >= 0; i--){
+            int minCost = Integer.MAX_VALUE;
+
+                for(int j = i; j < s.length(); j++){    
+                    if(isPalindrome(s,i,j)){
+                        int cost = 1 + dp[j+1];
+                        minCost = Math.min(cost,minCost);
+                    }
+                }
+            dp[i] = minCost;
+        
+        }
+        
+        for(int i : dp){
+            System.out.print(i+" ");
+        }
+        return dp[0]-1;
     }
     
-    private int helper(int i, String s,Integer[] dp){
-        
-        if(i == s.length()){
-            return 0;
-        }
-        
-        
-        if(dp[i] != null){
-            return dp[i];
-        }
-        
-        int minCost = Integer.MAX_VALUE;
-        
-        for(int j = i; j < s.length(); j++){    
-            if(isPalindrome(s,i,j)){
-                int cost = 1 + helper(j+1,s,dp);
-                minCost = Math.min(cost,minCost);
-            }
-        }
-        
-        
-        return dp[i]=  minCost;
-    }
+    
     
     private boolean isPalindrome(String s, int i, int j){
         while(i <= j){
