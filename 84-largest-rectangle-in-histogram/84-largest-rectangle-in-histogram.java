753727eade123f0ad4667921c226f40a5a21a9a7
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-18 16:33:20 +0530

    Time: 167 ms (63.31%), Space: 79.6 MB (77.66%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,56 +1,23 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        
-        int[] sol = nextSmallestonLeft(heights);
-        int[] sor = nextSmallestonRight(heights);
-        
-        int max = 0;
-        for(int i = 0; i < heights.length; i++){
-            int w = sor[i] - sol[i] -1;
-            int area = heights[i]*w;
-            max = Math.max(area,max);
-        }
-        
-        return max;
-    }
-    
-    private int[] nextSmallestonLeft(int[] arr){
-        int[] ans = new  int[arr.length];
         Stack<Integer> st = new Stack<>();
-        
-        for(int i = arr.length-1; i>= 0; i--){
-            while(st.size() > 0 && arr[st.peek()] > arr[i]){
-                ans[st.pop()] = i;
+        int maxi = 0;
+        int n = heights.length;
+        for(int i = 0; i <= n; i++){
+            while(st.size() > 0 && (i == n || heights[st.peek()] >= heights[i])){
+                int h = heights[st.pop()];
+                
+                int width = 0;
+                
+                if(st.size() == 0) width = i;
+                else width = i - st.peek()-1;
+                
+                maxi = Math.max(maxi,h*width);
             }
-            
             st.push(i);
-            
         }
         
-        while(st.size() != 0){
-            ans[st.pop()] = -1;
-        }
         
-        return ans;
-    }
-    
-     private int[] nextSmallestonRight(int[] arr){
-        int[] ans = new  int[arr.length];
-        Stack<Integer> st = new Stack<>();
-        
-        for(int i = 0; i < arr.length; i++){
-            while(st.size() > 0 && arr[st.peek()] > arr[i]){
-                ans[st.pop()] = i;
-            }
-            
-            st.push(i);
-            
-        }
-        
-        while(st.size() != 0){
-            ans[st.pop()] = arr.length;
-        }
-        
-        return ans;
+        return maxi;
     }
 }
