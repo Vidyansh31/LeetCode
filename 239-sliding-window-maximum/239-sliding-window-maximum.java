3ba5bfa7158cc7caaaac8b623c8e9ea112ca8f15
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-07-25 11:56:22 +0530

    Time: 25 ms (96.42%), Space: 156.7 MB (12.28%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -2,29 +2,26 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
         int[] ans = new int[n-k+1];
-        List<Integer> list = new ArrayList<>();
-        int i = 0;
-        int j = 0;
         
-        while(j < n){
-            while(list.size() > 0 && list.get(list.size()-1) < nums[j]){
-                    list.remove(list.size()-1);
-                }
-                
-            list.add(nums[j]);
-            if(j - i + 1 < k){
-                j++;
-            }
-            else if(j - i + 1 == k){
-                ans[i] = list.get(0);
-                if(nums[i] == list.get(0)){
-                    list.remove(0);
-                }
-                i++;
-                j++;
-            }
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n]; 
+        
+        leftMax[0] = nums[0];
+        rightMax[n-1] = nums[n-1];
+        
+        for(int i = 1; i < n; i++){
+            leftMax[i] = i%k == 0 ? nums[i] : Math.max(leftMax[i-1],nums[i]);
+            
+            int j = n-i-1;
+            rightMax[j] = j%k == 0 ? nums[j] : Math.max(rightMax[j+1],nums[j]);
+        }
+        
+        
+        for(int i = 0, j = 0; i+k <= n; i++){
+            ans[j++] = Math.max(rightMax[i],leftMax[i+k-1]);
         }
         
         return ans;
+        
     }
 }
