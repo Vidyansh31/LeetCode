e7962c3ec91a66d9a756fa73c137b174475f228a
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-01 09:42:28 +0530

    Time: 0 ms (100.00%), Space: 39 MB (94.26%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -4,14 +4,11 @@ class Solution {
         
         for(int i = m-1; i >= 0; i--){
             for(int j = n-1; j >= 0; j--){
-                if(i == m-1 && j == n-1){
+                if(i == m-1){
                     dp[i][j] = 1;
                 }
-                else if( j == n-1){
+                else if(j == n-1){
                     dp[i][j] = 1;
-                }
-                else if( i == m-1){
-                     dp[i][j] = 1;
                 }
                 else{
                     dp[i][j] = dp[i+1][j] + dp[i][j+1];
