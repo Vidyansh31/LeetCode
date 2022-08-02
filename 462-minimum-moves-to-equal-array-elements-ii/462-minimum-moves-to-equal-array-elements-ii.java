17ec4cba19a7f06d835fd0041cec6bcfb0c34011
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-02 14:55:00 +0530

    Time: 4 ms (91.61%), Space: 47 MB (5.85%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -5,14 +5,11 @@ class Solution {
         //Find median
         int low = 0;
         int high = nums.length-1;
-        
-        int mid = low + (high-low)/2;
-        
-        int median = nums[mid];
-        
         int count = 0;
-        for(int num : nums){
-            count += Math.abs(num-median);
+        while(low < high){
+            count += nums[high] - nums[low];
+            low++;
+            high--;
         }
         
         return count;
