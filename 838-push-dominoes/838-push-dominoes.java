f7f3b526a20fac0da19dbe2140220aba70e97ed1
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-04 13:27:12 +0530

    Time: 49 ms (29.07%), Space: 67.9 MB (19.79%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.java
+++ 838-push-dominoes/838-push-dominoes.java
@@ -1,67 +1,48 @@
 class Solution {
     public String pushDominoes(String dominoes) {
-        int n = dominoes.length();
-        int[] left = new int[n];
-        int[] right = new int[n];
+        dominoes = "L" + dominoes +"R";
+        int prev = 0;
         
-        left[n-1] = dominoes.charAt(n-1) == 'L' ? 1 : 0;
-        //left force
-        for(int i = n-2; i >= 0; i--){
-            char ch = dominoes.charAt(i);
-            
-            if(ch == 'R'){
-                left[i] = 0;
-            }
-            else if(ch == 'L'){
-                left[i] = 1;
-            }
-            else{
-                left[i] = left[i+1] > 0 ? left[i+1]+1 : 0;
-            }
-        }
+        char[] arr = dominoes.toCharArray();
         
-        //right force
-        right[0] = dominoes.charAt(0) == 'R' ? 1 : 0;
-        
-        for(int i = 1; i < n; i++){
-             char ch = dominoes.charAt(i);
-            
-            if(ch == 'L'){
-                right[i] = 0;
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] == 'L'){
+                if(arr[prev] == 'L'){
+                    for(int x = prev+1; x < i; x++){
+                        arr[x] = 'L';
+                    }
+                }
+                else if(arr[prev] == 'R'){
+                    int lo = prev+1;
+                    int hi = i-1;
+                    while(lo < hi){
+                        arr[lo] = 'R';
+                        arr[hi] = 'L';
+                        lo++;
+                        hi--;
+                    }
+                }
+                prev = i;
             }
-            else if(ch == 'R'){
-                right[i] = 1;
-            }
-            else{
-                right[i] = right[i-1] > 0 ? right[i-1]+1 : 0;
+            else if(arr[i] == 'R'){
+                if(arr[prev] == 'L'){
+                    // do nothing
+                }
+                else if(arr[prev] == 'R'){
+                    for(int x = prev+1; x < i; x++){
+                        arr[x] = 'R';
+                    }
+                }
+                prev = i;
             }
         }
         
         StringBuilder sb = new StringBuilder("");
-        for(int i = 0; i < n; i++){
-            if(left[i] == 0 && right[i] == 0){
-                sb.append(".");
-            }
-            else if(left[i] == 0){
-                sb.append("R");
-            }
-            else if(right[i] == 0){
-                sb.append("L");
-            }
-            else{
-                if(left[i] < right[i]){
-                    sb.append("L");
-                }
-                else if(left[i] > right[i]){
-                    sb.append("R");
-                }
-                else{
-                    sb.append(".");
-                }
-            }
+        
+        for(int i = 1; i < arr.length-1; i++){
+            sb.append(arr[i]+"");
         }
-       
         
         return sb.toString();
     }
-} 
+}
