7228c7319bcbe731aa692e5f661b16e30160909e
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-13 23:34:26 +0530

    Time: 20 ms (59.94%), Space: 45.6 MB (91.63%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,19 +1,20 @@
 class Solution {
     public class Pair{
-        int i; 
-        int j;
+        int x;
+        int y;
         int level;
         
-        Pair(int i, int j, int level){
-            this.i=i;
-            this.j=j;
+        Pair(int x, int y, int level){
+            this.x = x;
+            this.y = y;
             this.level = level;
         }
     }
     
     public int[][] updateMatrix(int[][] mat) {
-        
         Queue<Pair> qu = new LinkedList<>();
+        boolean[][] vis = new boolean[mat.length][mat[0].length];
+        
         for(int i = 0; i < mat.length; i++){
             for(int j = 0; j < mat[0].length; j++){
                 if(mat[i][j] == 0){
@@ -22,31 +23,33 @@ class Solution {
             }
         }
         
-        int[][] ans = new int[mat.length][mat[0].length];
-        
-        boolean[][] visited = new boolean[mat.length][mat[0].length];
         while(qu.size() > 0){
             Pair rem = qu.remove();
             
-            if(visited[rem.i][rem.j] == true){
+            if(vis[rem.x][rem.y] == true){
                 continue;
             }
-            visited[rem.i][rem.j] = true;
+            vis[rem.x][rem.y] = true;
             
-            ans[rem.i][rem.j] = rem.level;
+            mat[rem.x][rem.y] = rem.level;
             
-            addN(rem.i-1,rem.j,rem.level+1,qu,mat,visited);
-            addN(rem.i,rem.j+1,rem.level+1,qu,mat,visited);
-            addN(rem.i+1,rem.j,rem.level+1,qu,mat,visited);
-            addN(rem.i,rem.j-1,rem.level+1,qu,mat,visited);
+            addChild(mat,qu,vis,rem.x-1,rem.y,rem.level+1);
+            addChild(mat,qu,vis,rem.x,rem.y+1,rem.level+1);
+            addChild(mat,qu,vis,rem.x+1,rem.y,rem.level+1);
+            addChild(mat,qu,vis,rem.x,rem.y-1,rem.level+1);
         }
         
-        
-        return ans;
+        return mat;
     }
     
-    public void addN(int i, int j, int level, Queue<Pair> qu, int[][] mat , boolean[][] visited){
-        if(i < 0 || j < 0 || i >= mat.length || j >= mat[0].length || mat[i][j] == 0 || visited[i][j] == true ){
+    public void addChild(int[][] mat, Queue<Pair> qu, boolean[][] vis, int i, int j, int level){
+        if( i < 0 || i >= mat.length || j < 0 || j >= mat[0].length){
+            return;
+        }
+        else if(vis[i][j]){
+            return;
+        }
+        else if(mat[i][j] == 0){
             return;
         }
         
