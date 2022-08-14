6393447d41dfc738bc946973d719631386ae9932
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-14 17:34:55 +0530

    Time: 2 ms (63.69%), Space: 43.4 MB (85.52%) - LeetHub

--- 1034-coloring-a-border/1034-coloring-a-border.java
+++ 1034-coloring-a-border/1034-coloring-a-border.java
@@ -1,60 +1,67 @@
 class Solution {
+    
     public class Pair{
-        int i;
-        int j;
-        boolean border;
+        int x; 
+        int y;
+        int color;
         
-        Pair(int i, int j, boolean border){
-            this.i = i;
-            this.j = j;
-            this.border = border;
+        Pair(int x, int y, int color){
+            this.x = x;
+            this.y = y;
+            this.color = color;
         }
     }
     
     public int[][] colorBorder(int[][] grid, int row, int col, int color) {
-        Queue<Pair> qu = new LinkedList<>();
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        
         int boxColor = grid[row][col];
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
         
-        qu.add(new Pair(row,col,border(row,col,grid,boxColor)));
-        
-        List<Pair> ans = new ArrayList<>();
-        
-        
-        while(qu.size() > 0){
-            Pair rem = qu.remove();
-            
-            if(visited[rem.i][rem.j] == true){
-                continue;
-            }
-            visited[rem.i][rem.j] = true;
-            
-            if(rem.border == true){
-                ans.add(rem);
-            }
-            
-            addN(rem.i-1,rem.j,grid,visited,qu,boxColor);
-            addN(rem.i,rem.j+1,grid,visited,qu,boxColor);
-            addN(rem.i+1,rem.j,grid,visited,qu,boxColor);
-            addN(rem.i,rem.j-1,grid,visited,qu,boxColor);
-        }
-        
-        for(Pair p : ans){
-            grid[p.i][p.j] = color;
-        }
+        traversalBFS(grid,vis,row,col,boxColor,color);
         
         return grid;
     }
     
-    public void addN(int i, int j, int[][] grid, boolean[][] visited, Queue<Pair> qu, int boxColor){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || visited[i][j] == true || grid[i][j] != boxColor){
+    public void traversalBFS(int[][] grid, boolean[][] vis, int i, int j, int boxColor, int color){
+        Queue<Pair> qu = new LinkedList<>();
+        qu.add(new Pair(i,j,boxColor));
+        List<Integer> ans = new ArrayList<>();
+        while(qu.size() > 0){
+            Pair rem = qu.remove();
+            
+            if(vis[rem.x][rem.y] == true){
+                continue;
+            }
+            vis[rem.x][rem.y] = true;
+            
+            if(isBorder(grid,rem.x,rem.y, boxColor)){
+                ans.add(rem.x);
+                ans.add(rem.y);
+            }
+            
+            addChild(grid,qu,vis,rem.x-1,rem.y,boxColor);
+            addChild(grid,qu,vis,rem.x,rem.y+1,boxColor);
+            addChild(grid,qu,vis,rem.x+1,rem.y,boxColor);
+            addChild(grid,qu,vis,rem.x,rem.y-1,boxColor);
+        }
+        
+        for(int k = 0; k < ans.size(); k = k + 2){
+            int x = ans.get(k);
+            int y = ans.get(k+1);
+            
+            grid[x][y] = color;
+        }
+    }
+    
+    public void addChild(int[][] grid, Queue<Pair> qu, boolean[][] vis, int i, int j, int boxColor){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || vis[i][j] == true || grid[i][j] != boxColor){
             return;
         }
         
-        qu.add(new Pair(i,j,border(i,j,grid,boxColor)));
+        qu.add(new Pair(i,j,boxColor));
     }
     
-    public boolean border(int i, int j , int[][] grid, int boxColor ){
+   public boolean isBorder(int[][] grid, int i, int j, int boxColor ){
          if(i == 0 || j == 0 || i == grid.length-1 || j == grid[0].length-1){
             return true; 
         }
