d3ab76aa1761e7796bb5ebe5195c7155476e25d1
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-15 22:51:57 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (95.12%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-        HashMap<Character,Integer> map = new HashMap<>();
+        // HashMap<Character,Integer> map = new HashMap<>();
+        int[] map = new int[26];
         
         int k = 0;
         for(char ch : order.toCharArray()){
-            map.put(ch,k++);
+            map[ch-'a'] = k++;
         }
         
         for(int i = 0; i < words.length-1; i++){
@@ -20,14 +21,14 @@ class Solution {
         return true;
     }
     
-    public boolean compare(String word1, String word2,HashMap<Character,Integer> map){
+    public boolean compare(String word1, String word2, int[] map){
         for(int j = 0; j < Math.min(word1.length(),word2.length()); j++){
                 char ch1 = word1.charAt(j);
                 char ch2 = word2.charAt(j);
                 
                 if(ch1 != ch2){
-                    int idx1 = map.get(ch1);
-                    int idx2 = map.get(ch2);
+                    int idx1 = map[ch1 - 'a'];
+                    int idx2 = map[ch2 - 'a'];
                     
                     if(idx1 > idx2){
                         return false;
