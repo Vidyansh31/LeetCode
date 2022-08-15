e2b50c62096e7da71b433297bb7039f6717a270c
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-15 21:23:43 +0530

    Time: 60 ms (6.07%), Space: 44 MB (6.70%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null){
+            return null;
+        }
+        
+        Node[] visited = new Node[101];
+        return helper(node, visited);
+    }
+    
+    public Node helper(Node node, Node[] visited){
+        Node nodeClone = new Node(node.val);
+        visited[node.val] = nodeClone;
+        
+        for(Node nbr : node.neighbors){
+            if(visited[nbr.val] == null){
+                Node nbrClone = helper(nbr,visited);
+                nodeClone.neighbors.add(nbrClone);
+            }
+            else{
+                Node nbrClone = visited[nbr.val];
+                nodeClone.neighbors.add(nbrClone);
+            }
+        }
+        
+        return nodeClone;
+    }
+}
