72a5bc39e65802702125db408065f33f550c3694
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-16 16:09:14 +0530

    Time: 2 ms (69.03%), Space: 41.8 MB (18.21%) - LeetHub

--- 263-ugly-number/263-ugly-number.java
+++ 263-ugly-number/263-ugly-number.java
@@ -8,27 +8,11 @@ class Solution {
             return false;
         }
         
-        while(n != 1){
-            if(n%2 == 0){
-                while(n%2 == 0){
-                    n /= 2;
-                }
-            }
-            else if(n%3 == 0){
-                while(n%3 == 0){
-                    n /= 3;
-                }
-            }
-            else if(n%5 == 0){
-                while(n%5 == 0){
-                    n /= 5;
-                }
-            }
-            else{
-                return false;
-            }
-        }
         
-        return true;
+        while(n%2 == 0) n /=2;
+        while(n%3 == 0) n /= 3;
+        while(n%5 == 0) n /= 5;
+        
+        return n == 1;
     }
 }
