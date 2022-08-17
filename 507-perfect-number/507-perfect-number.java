05eb61fbea8b4f3153293bd3c6fd3fa9dcaa708e
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-18 01:09:47 +0530

    Time: 3 ms (47.87%), Space: 40.6 MB (71.57%) - LeetHub

--- 507-perfect-number/507-perfect-number.java
+++ 507-perfect-number/507-perfect-number.java
@@ -1,15 +1,18 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
+        if(num == 1){
+            return false;
+        }
         int divisors = num/2;
-        int sum = 0;
-        
-        while(divisors > 0){
-            if(num%divisors == 0){
-                sum += divisors;
+        int sum = 1;
+       
+        for(int i = 2; i < Math.sqrt(num); i++){
+            if(num%i == 0){
+                sum += i + num/i;
             }
-            divisors--;
         }
         
+       
         return num == sum;
         
     }
