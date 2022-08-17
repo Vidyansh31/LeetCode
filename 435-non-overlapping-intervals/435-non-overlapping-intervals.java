553e5885219eec39f299cf9f394984dad2f45f62
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-17 15:02:34 +0530

    Time: 74 ms (85.02%), Space: 106.5 MB (6.14%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -5,25 +5,26 @@ class Solution {
         });
         
         int overlap = 0;
-        Stack<int[]> st = new Stack<>();
-        st.push(intervals[0]);
+        // Stack<int[]> st = new Stack<>();
+        // st.push(intervals[0]);
+        int end = intervals[0][1];
         
         for(int i = 1; i < intervals.length; i++ ){
-            int[] top = st.pop();
-            int s1 = top[0];
-            int e1 = top[1];
+            // int[] top = st.pop();
+            // int s1 = top[0];
+            // int e1 = top[1];
             
             int s2 = intervals[i][0];
             int e2 = intervals[i][1];
             
-            if(s2 < e1){
+            if(s2 < end){
                 overlap++;
-                st.push(new int[]{s1,Math.min(e1,e2)});
+               end = Math.min(end,e2);
             }
             else{
-                st.push(top);
-                st.push(intervals[i]);
+                end = e2;
             }
+           
         }
         
         return overlap;
