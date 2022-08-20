5c9626bca91a56a9e6d4894b5c7fa887ff854ccf
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-20 17:03:13 +0530

    Time: 252 ms (5.24%), Space: 127.6 MB (71.93%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,45 +1,81 @@
 class NumArray {
-    int[] nums;
-	int[] BIT;
-	int n;
-
-	public NumArray(int[] nums) {
-		this.nums = nums;
-
-		n = nums.length;
-		BIT = new int[n + 1];
-		for (int i = 0; i < n; i++)
-			init(i, nums[i]);
-	}
-
-	public void init(int i, int val) {
-		i++;
-		while (i <= n) {
-			BIT[i] += val;
-			i += (i & -i);
-		}
-	}
-
-	void update(int i, int val) {
-		int diff = val - nums[i];
-		nums[i] = val;
-		init(i, diff);
-	}
-
-	public int getSum(int i) {
-		int sum = 0;
-		i++;
-		while (i > 0) {
-			sum += BIT[i];
-			i -= (i & -i);
-		}
-		return sum;
-	}
-
-	public int sumRange(int i, int j) {
-		return getSum(j) - getSum(i - 1);
-	}
-
+    class Node{
+        int st;
+        int se;
+        Node left;
+        Node right;
+        int val;
+    }
+    
+    Node root;
+    
+    Node construct(int[] nums, int lo, int hi){
+        if(lo == hi){
+            Node base = new Node();
+            base.st = base.se = lo;
+            base.left = base.right = null;
+            base.val = nums[lo];
+            return base;
+        }
+        
+        Node node = new Node();
+        node.st = lo;
+        node.se = hi;
+        
+        int mid = (lo+hi)/2;
+        
+        node.left = construct(nums,lo,mid);
+        node.right = construct(nums,mid+1,hi);
+        
+        node.val += node.left.val + node.right.val;
+        
+        return node;
+    }
+    
+    public NumArray(int[] nums) {
+        root = construct(nums,0,nums.length-1);
+    }
+    
+    void updateVal(Node node, int idx, int val){
+        if(node.st == node.se){
+            node.val = val;
+            return;
+        }
+        
+        int mid = (node.st + node.se)/2;
+        if(idx <= mid){
+            updateVal(node.left,idx,val);
+        }
+        else{
+            updateVal(node.right,idx,val);
+        }
+        
+        node.val = 0;
+        node.val = node.left.val + node.right.val;
+    }
+    
+    public void update(int index, int val) {
+        updateVal(root,index,val);
+    }
+    
+    int query(Node node, int qs, int qe){
+        if(qs > node.se || qe < node.st){
+            return 0;
+        }
+        else if(node.st >= qs && node.se <= qe){
+            return node.val;
+        }
+        else{
+            int lval = query(node.left,qs,qe);
+            int rval = query(node.right,qs,qe);
+            
+            return lval+rval;
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        return query(root,left,right);
+    }
 }
 
 /**
