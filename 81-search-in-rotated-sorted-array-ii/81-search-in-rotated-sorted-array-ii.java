e4de4dd4ec388ec0b18216c33f93b56f2f09ca18
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-21 16:35:36 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (93.85%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -4,38 +4,38 @@ class Solution {
         int hi = nums.length-1;
         
         while(lo <= hi){
-            //Avoid Duplicates
+            //avoiding duplicates
             while(lo < hi && nums[lo] == nums[lo+1]){
                 lo++;
             }
             
-            while(lo < hi && nums[hi] == nums[hi-1]){
+            while(lo < hi && nums[hi-1] == nums[hi]){
                 hi--;
             }
             
-            //Now this question is same as Search in Rotated Array
-            int mid = (hi+lo)/2;
+            //Now this is same as Search in rotated Array
+            int mid = (lo+hi)/2;
             
             if(nums[mid] == target){
                 return true;
             }
-            
-            if(nums[lo] <= nums[mid]){
-                if(target >= nums[lo] && nums[mid] > target){
-                    hi = mid-1;
+            else if(nums[lo] <= nums[mid]){ // low to high is sorted
+                if(nums[lo] <= target && nums[mid] > target){
+                     hi = mid-1;
                 }
                 else{
                     lo = mid+1;
                 }
             }
             else{
-                 if(target <= nums[hi] && nums[mid] < target){
-                    lo = mid+1;
+                if(nums[hi] >= target && nums[mid] < target){ // mid to high is sorted
+                     lo = mid+1;
                 }
                 else{
                     hi = mid-1;
                 }
             }
+            
         }
         
         return false;
