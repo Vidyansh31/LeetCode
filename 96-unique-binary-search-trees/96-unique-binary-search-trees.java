55af5ba9d95e621b5775e15b3fa6b18ad719c088
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-08-29 17:47:55 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (97.03%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,25 +1,26 @@
 class Solution {
     public int numTrees(int n) {
-        // n = 1 2 3 4 5 ...
-        //sol= 1 2 5 14 42 ... this series in catalan series
-        
         int[] dp = new int[n+1];
         dp[0] = 1;
-        return helper(dp,n);
+        return helper(n,dp);
     }
     
-    private int helper(int[] dp, int n){
-        if(n==1) return dp[n] = 1;
-        if(n==2) return dp[n] = 2;
+    public int helper(int n , int[] dp){
+        if(n == 1) return 1;
+        if(n == 2) return 2;
         
         if(dp[n] != 0){
             return dp[n];
         }
-        int ans = 0;
+        
+        int total = 0;
         for(int i = 1; i <= n; i++){
-            ans += helper(dp,i-1)*helper(dp,n-i);
+            int left = helper(i-1,dp);
+            int right = helper(n-i,dp);
+            
+            total += left*right;
         }
         
-        return dp[n] = ans;
+        return dp[n] = total;
     }
 }
