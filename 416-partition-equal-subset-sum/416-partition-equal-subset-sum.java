1045484611f8759648cb1cf0fff9631ff856a2ef
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-09-01 18:20:24 +0530

    Time: 83 ms (45.50%), Space: 54.4 MB (47.29%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -5,38 +5,39 @@ class Solution {
             sum += num;
         }
         
-        if(sum%2 != 0){
+        if(sum%2 == 1){
             return false;
         }
-        //we just have to search for a subset whose sum is equal to sum/2;
         
         sum = sum/2;
         
         boolean[] prev = new boolean[sum+1];
+        
+//         for(int i = 0; i < dp.length; i++){
+//             dp[i][0] = true;
+//         }
+        
+//         for(int i = 1; i < dp[0].length; i++){
+//             dp[0][i] = false;
+//         }
         prev[0] = true;
         
-        
-        
-        for(int i = 1; i < nums.length; i++){
+        for(int i = 1; i <= nums.length; i++){
             boolean[] curr = new boolean[sum+1];
             curr[0] = true;
             for(int j = 1; j <= sum; j++){
-                 boolean notPick = prev[j];
-                 boolean pick = false;
-
-                 if(nums[i] <= j){
-                    pick = prev[j-nums[i]];
-                 }
-
-                 curr[j] =  pick || notPick;
+                boolean notPick = prev[j];
+                
+                boolean pick = false;
+                
+                if(j >= nums[i-1]) pick = prev[j-nums[i-1]];
+                
+                curr[j] = pick || notPick;
             }
-            
             prev = curr;
+
         }
         
-        return prev[sum];
-        
+       return prev[sum];
     }
-    
-    
 }
