078f79fdb2ae3fc074a13c137d8f514082b00897
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-09-05 23:32:13 +0530

    Time: 127 ms (35.44%), Space: 48.8 MB (64.82%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.java
+++ 16-3sum-closest/16-3sum-closest.java
@@ -4,13 +4,12 @@ class Solution {
         int diff = Integer.MAX_VALUE;
         int ans = 0;
         for(int i = 0; i < nums.length-2; i++){
-            
             int lo = i+1;
             int hi = nums.length-1;
             while(lo < hi){
                 int currSum = nums[lo] + nums[hi] + nums[i];
                 
-                if(Math.abs(currSum -target) < diff){
+                if(Math.abs(currSum - target) < diff){
                     diff = Math.abs(currSum - target);
                     ans = currSum;
                 }
