2d1429e2821e79717b2c68985627b6ccd5e342ad
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-09-09 14:48:51 +0530

    Time: 196 ms (42.12%), Space: 89.6 MB (38.12%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -8,48 +8,47 @@ class Solution {
             this.level = level;
         }
     }
-    
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         if(wordList.contains(endWord) == false){
             return 0;
         }
         
-        //Add all words int set from wordList
+        Queue<Pair> qu = new LinkedList<>();
+        qu.add(new Pair(beginWord,1));
+        
         Set<String> set = new HashSet<>();
         for(String word : wordList){
             set.add(word);
         }
         
-        //Start bfs with beginWord
-        Queue<Pair> qu = new LinkedList<>();
-        qu.add(new Pair(beginWord,1));
-        
         while(qu.size() > 0){
-            Pair rem = qu.remove();
+            Pair p = qu.remove();
+            String rem = p.word;
             
-            String str = rem.word;
-            //Loop over every character of str and change it by [a,z]
-            // if change word is present in set, then
-            for(int i = 0; i < str.length(); i++){
-                char wh = str.charAt(i);
-                for(int k = 'a'; k <= 'z'; k++){
-                    char arr[] = str.toCharArray();
-                    arr[i] = (char) k;
+            
+            for(int i = 0; i < rem.length(); i++){
+                char[] carr = rem.toCharArray();
+                
+                for(char ch = 'a'; ch <= 'z'; ch++){
+                    if(ch != rem.charAt(i)){
+                        carr[i] = ch;
                         
-                    String s = new String(arr);
-            
-                    if(set.contains(s)){
-                        // check if that word is equals to endWord if equal return curr level + 1
-                        // else add in queue and remove from set
-                        if(s.equals(endWord)){
-                            return rem.level+1;
+                        String word = new String(carr);
+                        // System.out.println(word);
+                        
+                        if(word.equals(endWord)){
+                            return p.level+1;
                         }
-                        qu.add(new Pair(s,rem.level+1));
-                        set.remove(s);
                         
+                        if(set.contains(word)){
+                            qu.add(new Pair(word,p.level+1));
+                            set.remove(word);
+                            
+                            
+                        }
                     }
                 }
-            }
+            }   
         }
         
         return 0;
