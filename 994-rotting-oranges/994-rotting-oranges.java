4ec9f4fe20494ebc44d87daf6c1e6fb46d718d69
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-09-10 12:33:00 +0530

    Time: 4 ms (46.26%), Space: 42.9 MB (58.56%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,24 +1,25 @@
 class Solution {
-    
-    
-    public class Pair{
-        int x; int y; int t;
+    class Pair{
+        int x;
+        int y;
+        int time;
         
-        Pair(int x, int y, int t){
+        Pair(int x, int y, int time){
             this.x = x;
             this.y = y;
-            this.t = t;
+            this.time = time;
         }
     }
-    
     public int orangesRotting(int[][] grid) {
-        int fresh = 0;
-        int time = 0;
-        Queue<Pair> qu = new LinkedList<>();
-        boolean[][] vis = new boolean[grid.length][grid[0].length];
+        int m = grid.length;
+        int n = grid[0].length;
         
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
+        boolean[][] vis = new boolean[m][n];
+        Queue<Pair> qu = new LinkedList<>();
+        int fresh = 0;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(grid[i][j] == 2){
                     qu.add(new Pair(i,j,0));
                 }
@@ -28,22 +29,27 @@ class Solution {
             }
         }
         
+        int time = 0;
+        
         while(qu.size() > 0){
             Pair rem = qu.remove();
             
-            if(vis[rem.x][rem.y] == true){
+            if(vis[rem.x][rem.y]){
                 continue;
             }
+            
             vis[rem.x][rem.y] = true;
-            time = rem.t;
+            
+            time = Math.max(time,rem.time);
             if(grid[rem.x][rem.y] == 1){
                 fresh--;
             }
             
-            addChild(grid,qu,vis,rem.x-1,rem.y,rem.t+1);
-            addChild(grid,qu,vis,rem.x,rem.y+1,rem.t+1);
-            addChild(grid,qu,vis,rem.x+1,rem.y,rem.t+1);
-            addChild(grid,qu,vis,rem.x,rem.y-1,rem.t+1);
+            //add children
+            addChild(qu,vis,grid,rem.x-1,rem.y,rem.time + 1);
+            addChild(qu,vis,grid,rem.x,rem.y+1,rem.time + 1);
+            addChild(qu,vis,grid,rem.x+1,rem.y,rem.time + 1);
+            addChild(qu,vis,grid,rem.x,rem.y-1,rem.time + 1);
         }
         
         if(fresh == 0){
@@ -53,17 +59,12 @@ class Solution {
         return -1;
     }
     
-    public void addChild(int[][] grid, Queue<Pair> qu,boolean[][] vis , int i, int j, int t){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length){
-            return;
-        }
-        else if(vis[i][j] == true){
-            return;
-        }
-        else if(grid[i][j] != 1){
+    
+    public void addChild(Queue<Pair> qu, boolean[][] vis, int[][] grid, int i, int j, int time){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || vis[i][j] == true || grid[i][j] != 1){
             return;
         }
         
-        qu.add(new Pair(i,j,t));
+        qu.add(new Pair(i,j,time));
     }
 }
