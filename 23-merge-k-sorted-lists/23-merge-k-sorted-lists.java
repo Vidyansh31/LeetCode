e6b796cc5fb162047b2e8476a7a8f8ec115feb51
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-09-10 13:31:59 +0530

    Time: 11 ms (34.80%), Space: 47.5 MB (40.57%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,56 +10,30 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0){
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> {
+            return a.val - b.val;
+        });
+        
+        if(lists.length == 0 ){
             return null;
         }
         
-        if(lists.length == 1){
-            return lists[0];
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        for(int i = 0; i < lists.length; i++){
+            if(lists[i] != null)
+                pq.add(lists[i]);
         }
         
-
-       return helper1(lists,0,lists.length-1);
-    }
-    
-    public ListNode helper1(ListNode[] lists, int st, int end){
-        if(st > end) return null;
-        if(st == end) return lists[st];
-        int mid = (st+end)/2;
-        ListNode l1 = helper1(lists,st,mid);
-        ListNode l2 = helper1(lists,mid+1,end);
-        
-        return merge(l1,l2);
-    }
-    
-    public ListNode merge( ListNode one , ListNode two){
-        if(one == null || two == null){
-            return one == null ? two : one;
+        while(pq.size() > 0){
+            ListNode rem = pq.remove();
+            curr.next = rem;
+            curr = rem;
             
-        }
-        
-        ListNode dummy = new ListNode(-1);
-        ListNode prev = dummy;
-        
-        while(one != null && two != null){
-            if(one.val <= two.val){
-                prev.next = one;
-                prev = one;
-                one = one.next;
+            if(rem.next != null){
+                pq.add(rem.next);
             }
-            else{
-                 prev.next = two;
-                prev = two;
-                two = two.next;
-            }
-        }
-        
-        if(one == null){
-            prev.next = two;
-        }
-        
-        if(two == null){
-            prev.next = one;
         }
         
         return dummy.next;
