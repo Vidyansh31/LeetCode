f127130b8fb75a8700b3594ed017672c6df70d8a
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-09-18 13:34:26 +0530

    Time: 5 ms (87.38%), Space: 44.1 MB (75.14%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,25 +1,27 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        Integer[][] dp = new Integer[nums.length][nums.length];
-        return helper(nums,0,-1,dp);
-    }
-    
-    public int helper(int[] nums, int idx, int prev_idx, Integer[][] dp){
-        if(idx == nums.length){
-            return 0;
+        int n = nums.length;
+        int[] tails = new int[n];
+        int len = 1;
+        tails[0] = nums[0];
+        
+        for(int i = 1; i < n; i++){
+            if(nums[i] > tails[len-1]){
+                len++;
+                tails[len-1] = nums[i];
+            }
+            else{
+                int idx = Arrays.binarySearch(tails,0,len-1,nums[i]);
+                
+                if(idx < 0){
+                    idx = -idx;
+                    idx = idx -1 ;
+                }
+                
+                tails[idx] = nums[i];
+            }
         }
         
-        if(dp[idx][prev_idx+1] != null){
-            return dp[idx][prev_idx+1];
-        }
-        
-        int NotPick = helper(nums,idx+1,prev_idx,dp);
-        int pick = Integer.MIN_VALUE;
-        
-        if(prev_idx == -1 || nums[prev_idx] < nums[idx]) pick = 1 + helper(nums,idx+1,idx,dp);
-        
-        
-        return dp[idx][prev_idx+1] = Math.max(pick,NotPick);
-        
+        return len;
     }
 }
