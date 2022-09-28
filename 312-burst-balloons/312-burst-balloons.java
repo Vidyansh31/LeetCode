6c2fd2917a367a1974c56ac235e84ed12dfdaeca
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-09-28 13:11:59 +0530

    Time: 123 ms (59.80%), Space: 43.2 MB (47.52%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.java
+++ 312-burst-balloons/312-burst-balloons.java
@@ -1,52 +1,24 @@
 class Solution {
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        int[] num = new int[n+2];
-        num[0] = 1;
-        for(int i = 1; i <= n; i++){
-            num[i] = nums[i-1];
-        }
-        num[n+1] = 1;
-        int[][] dp = new int[n+2][n+2];
+        int[][] dp = new int[n][n];
         
-        for(int i = n; i >= 1; i--){
-            for(int j = 1; j<=n; j++){
-                if(i > j){
-                    continue;
-                }
+        for(int g = 0; g < n; g++){
+            for(int i = 0, j = g ; j < n; j++,i++){
                 
-                int maxCoin = Integer.MIN_VALUE;
-                for(int k = i; k <= j ; k++){
-                    int coin = num[i-1]*num[k]*num[j+1] + dp[i][k-1] + dp[k+1][j];
-                    maxCoin = Math.max(coin,maxCoin);
+                for(int lk = g + 1, rk = 1; lk >= 1; lk--, rk++){
+                    int left = (lk == g + 1 ? 0 : dp[i][j-lk]);
+                    int right = (rk == g+1 ? 0 : dp[i+rk][j]);
+                    int factor = (i - 1 >= 0 ? nums[i-1] : 1) *
+                                    nums[i + rk - 1] *
+                                (j + 1 < n ? nums[j+1] : 1);
+                    
+                    dp[i][j] = Math.max(dp[i][j], left+right+factor);
                 }
-                
-                dp[i][j] = maxCoin;
             }
         }
-        return dp[1][n];
-    }
-    
-    private int helper(int st, int end, int[] nums, Integer[][] dp){
-        if(st > end){
-            return 0;
-        }
         
-        if(dp[st][end] != null){
-            return dp[st][end];
-        }
         
-        int maxCoin = Integer.MIN_VALUE;
-        
-        for(int k = st; k <= end; k++){
-            int prev = nums[st-1];
-            int next = nums[end+1];
-            
-            int coin = prev*nums[k]*next + helper(st,k-1,nums,dp) + helper(k+1,end,nums,dp);
-            
-            maxCoin = Math.max(coin,maxCoin);
-        }
-        
-        return dp[st][end] = maxCoin;
+        return dp[0][n-1];
     }
 }
