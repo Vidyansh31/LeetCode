7eb7ace33331f27526ea1e59f84e596d790e911e
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-10-28 23:51:40 +0530

    Time: 11 ms (85.78%), Space: 55.7 MB (61.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,30 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-       HashMap<HashMap<Character,Integer>, List<String>> map = new HashMap<>();
-        
-        for(String str : strs){
-            HashMap<Character,Integer> freq = new HashMap<>();
-            
-            for(char ch : str.toCharArray()){
-                freq.put(ch, freq.getOrDefault(ch,0)+1);
-            }
-            
-            if(map.containsKey(freq) == false){
-                map.put(freq, new ArrayList<>());
-            }
-            
-            map.get(freq).add(str);
+      if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(s);
         }
-        
-        
-        List<List<String>> ans = new ArrayList<>();
-        
-        for(HashMap<Character,Integer> key : map.keySet()){
-            ans.add(map.get(key));
-        }
-        
-        
-        return ans;
+        return new ArrayList<>(map.values());
         
     }
 }
