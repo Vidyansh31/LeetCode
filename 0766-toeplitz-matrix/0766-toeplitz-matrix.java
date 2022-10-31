c414eb9b14c70a8f975728460454b7b5f75892c7
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-10-31 20:58:23 +0530

    Time: 1 ms (93.83%), Space: 45.7 MB (54.67%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,18 +1,10 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-       HashMap<Integer,Set<Integer>> map = new HashMap<>();
+       
         
-       for(int i = 0; i < matrix.length; i++){
-           for(int j = 0; j < matrix[0].length; j++){
-               int gap = i-j;
-               
-               if(map.containsKey(gap) == false){
-                   map.put(gap, new HashSet<>());
-               }
-               
-               map.get(gap).add(matrix[i][j]);
-               
-               if(map.get(gap).size() > 1){
+       for(int i = 0; i < matrix.length-1; i++){
+           for(int j = 0; j < matrix[0].length-1; j++){
+               if(matrix[i][j] != matrix[i+1][j+1]){
                    return false;
                }
            }
