1abe6ed221a30846b1fe642b51a377bff959f81a
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2022-11-23 19:21:38 +0530

    Time: 43 ms (5.77%), Space: 49 MB (7.97%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>() ;
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                 if (board[i][j] != '.') {
+                String b = "(" + board[i][j] + ")";
+                if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
+                    return false;
+            }
+            }
+        }
+        
+        return true;
+    }
+}
