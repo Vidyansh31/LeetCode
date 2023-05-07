15d370bc76620f569abcdeda76a9028dbe1cebe2
Vidyansh31 <87309383+Vidyansh31@users.noreply.github.com>
2023-05-07 13:15:57 +0530

    Time: 60 ms (85.91%), Space: 45 MB (31.44%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    return {
+        resetValue: init,
+        increment : function(){
+            return ++init;
+        },
+        decrement : function(){
+            return --init;
+        },
+        reset : function(){
+            init = this.resetValue;
+            return init;
+        }
+        
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
